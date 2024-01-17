a0cccea9ac96807117a0a76db34146ce24a27ecd
Gustavo Hell <gorpro2004@mail.ru>
2024-01-18 01:36:37 +0400

    project updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import style from './App.module.scss';
 import CurrenciesTable from './components/currenciesTable/CurrenciesTable';
 import Header from './components/header/Header';
@@ -6,6 +7,7 @@ import { useAppSelector } from './redux/hooks';
 
 function App() {
   const isDarkMode: boolean = useAppSelector(state => state.currenciesSlice.isDarkMode);
+
   return (
     <div className={isDarkMode ? style.darkMain : style.lightMain}>
       <Header />
